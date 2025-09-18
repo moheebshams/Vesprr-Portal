c8f2f7be33c3df69ee950a62452577d8c4d44750
Moheeb <moheebshams@gmail.com>
2025-09-17 23:36:55 -0700

    fixing

--- core/views.py
+++ core/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'page_title': 'Dashboard'})
 
 def courses(request):
-    return render(request, 'courses.html')
+    return render(request, 'courses.html', {'page_title': 'Courses'})
 
 def attendance(request):
-    return render(request, 'attendance.html')
+    return render(request, 'attendance.html', {'page_title': 'Attendance'})
 
 
